89c68f572df5eb42706f94871c8e46e6f68171cd
hoang-quy <quy2529@gmail.com>
2024-03-18 19:58:22 +0700

    implement employee dao

--- src/main/java/com/quy/springboot/cruddemo/dao/EmployeeDAO.java
+++ src/main/java/com/quy/springboot/cruddemo/dao/EmployeeDAO.java
@@ -1,4 +1,11 @@
 package com.quy.springboot.cruddemo.dao;
 
+import com.quy.springboot.cruddemo.entity.Employee;
+
+import java.util.List;
+
 public interface EmployeeDAO {
+
+    List<Employee> findAll();
+
 }
