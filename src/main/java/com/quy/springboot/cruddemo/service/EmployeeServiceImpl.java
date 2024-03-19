8831723d3fcba12212815091bc04caee49968c0a
hoang-quy <quy2529@gmail.com>
2024-03-19 09:22:59 +0700

    Add add save delete to sevice layer

--- src/main/java/com/quy/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/quy/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -2,6 +2,7 @@ package com.quy.springboot.cruddemo.service;
 
 import com.quy.springboot.cruddemo.dao.EmployeeDAO;
 import com.quy.springboot.cruddemo.entity.Employee;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,5 +18,22 @@ public class EmployeeServiceImpl implements EmployeeService{
     @Override
     public List<Employee> findAll() {
         return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(int theId) {
+        return employeeDAO.findById(theId);
+    }
+
+    @Transactional
+    @Override
+    public Employee save(Employee employee) {
+        return employeeDAO.save(employee);
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(int theId) {
+        employeeDAO.deleteById(theId);
     }
 }
