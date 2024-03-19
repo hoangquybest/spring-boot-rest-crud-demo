72a5054ed3149e41718866b08e362136a248908b
hoang-quy <quy2529@gmail.com>
2024-03-19 14:06:52 +0700

    crud with spring data rest

--- src/main/java/com/quy/springboot/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/quy/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,6 +2,12 @@ package com.quy.springboot.cruddemo.dao;
 
 import com.quy.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+// Default it will take plural form of Employee entity to define path, and some complex plural that it can not resolve
+// We can use @RepositoryRestResource to define path
+// http://localhost:8080/magic-api/members?page=1&size=3
+// http://localhost:8080/magic-api/members?sort=lastName,desc
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
