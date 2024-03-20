71c18742897ef020211f46988b7616e133784273
hoang-quy <quy2529@gmail.com>
2024-03-20 21:18:16 +0700

    spring security with default database schema

--- src/main/java/com/quy/springboot/cruddemo/security/DemoSecurityConfig.java
+++ src/main/java/com/quy/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -5,16 +5,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
+    // add support for JDBC ... no more hardcoded users
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+/*
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
 
@@ -38,6 +45,7 @@ public class DemoSecurityConfig {
 
         return new InMemoryUserDetailsManager(join, mary, lyly);
     }
+*/
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
